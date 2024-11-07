79977a526501345f9e518111140590152c88787b
john <you@example.com>
2024-11-07 15:44:44 +0530

    -----

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const AdminSocketId = require("./models/AdminSocketId")
 require("dotenv").config()        
 
 // Read Firebase credentials JSON file
-const firebaseCredentialsPath = path.join(__dirname, "firebaseConfig.js");
+const firebaseCredentialsPath = path.join(__dirname, "firebaseConfig.json");
 const firebaseCredentials = JSON.parse(fs.readFileSync(firebaseCredentialsPath, "utf-8"));
 
 firebaseAdmin.initializeApp({
